b7b89703d17b6c81571430ce5fb90a1d459dcabc
Rainbowcats999 <526erizov@frhsd.com>
2024-11-27 14:31:26 +0000

    Added more to js

--- script.js
+++ script.js
@@ -43,7 +43,37 @@ function shakeMagic8Ball () {
     {userQuestion}. The wise and wordous  8 ball has replied $(eightBall)`
 }
 
+const answers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes definitely.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
+];
 
+const ball = document.getElementById("ball");
+const answerText = document.getElementById("answer-text");
+const askButton = document.getElementById("askButton");
+
+askButton.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * answers.length);
+  answerText.textContent = answers[randomIndex];
+});
 
 
 
